ee6bef702ec6682f649edbcf38cbf6ccb8981f3a
Arijit Mondal <arijitm717@gmail.com>
2025-09-07 21:47:44 +0530

    update cors

--- todo-server/src/app.ts
+++ todo-server/src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://todo-app-b219.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
